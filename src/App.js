ada5e5a125031a4e258bd9920ea8514ff64b8e0e
Vladimir <falinvlad93@rambler.ru>
2023-01-18 21:42:41 +0300

    first commit: one screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import React, { useRef, useState, useEffect } from 'react';
+
+//Components
+const Screen1 = React.lazy(() => import ("./components/screen/Screen1")) ;
 
 function App() {
+
+  const [ preloader, setPreloader ] = useState(true);
+
+  const containerRef = useRef(null);
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        { preloader 
+          ? <div>
+              loading
+            </div>
+          : <LocomotiveScrollProvider
+              options={
+                {
+                  smooth: true,
+                  multiplier: 1, // скорость прокрутки скролла
+                  offset: [0, 0],
+                  class: 'is-inview'
+                  // ... all available Locomotive Scroll instance options 
+                }
+              }
+              watch={
+                [
+                  //..all the dependencies you want to watch to update the scroll.
+                  //  Basicaly, you would want to watch page/location changes
+                  //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+                ]
+              }
+              containerRef={containerRef}
+              >
+                  <div className="App" data-scroll-container  ref={containerRef}>
+                    <section
+                      data-scroll-section
+                      data-scroll-repeat="true"
+                      >
+                      <Screen1 />
+                    </section>
+                    <section data-scroll-section>
+                      <div style={{ width: `100vw`, height: `300vh` }}></div>
+                    </section>
+                </div>
+            </LocomotiveScrollProvider>
+        }
+    </>
   );
 }
 
---
+++ src/components/screen/Screen1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function Screen1() {
+  return (
+    <div className='screen1'>
+      <Spline scene="https://prod.spline.design/4WoBi1qClUh4EKvR/scene.splinecode" />
+    </div>
+  )
+}
